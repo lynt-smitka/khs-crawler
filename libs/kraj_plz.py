bd74c23bdaa5eb38460f9b5d6eb1c77258d31120
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-04-04 01:57:25 +0200

    Plzensky kraj: prevod na 2bit barvy a zvetseni pred OCR, uprava parametru Tesseractu

--- libs/kraj_plz.py
+++ libs/kraj_plz.py
@@ -13,33 +13,37 @@ class web:
     for px_x in range(0, w):
       for px_y in range(0, h):
         px = cropped.getpixel((px_x, px_y))
-        if px > (200, 255):
+        if px > (100, 255):
           cropped.putpixel((px_x, px_y), (255, 255))
+        if px < (100, 255):
+          cropped.putpixel((px_x, px_y), (0, 255))
+    cropped = cropped.resize([int(2 * s) for s in cropped.size], Image.NEAREST)
     return cropped
   
   def crawl(self):
+    tsconfig='--psm 7 digits'
     results=[]
     img = utils.get_img('http://www.khsplzen.cz/images/KHS/covid19/Plzensky_kraj.jpg')
     
-    val = image_to_string(self.imgcrop(img,294,285), config='digits')
+    val = int(image_to_string(self.imgcrop(img,294,285), config=tsconfig))
     results.append({ 'okres':'Plzeň-město', 'kraj': self.kraj, 'hodnota':val})
     
-    val = image_to_string(self.imgcrop(img,100,234), config='digits')
+    val = int(image_to_string(self.imgcrop(img,100,234), config=tsconfig))
     results.append({ 'okres':'Tachov', 'kraj': self.kraj, 'hodnota':val})
     
-    val = image_to_string(self.imgcrop(img,262,187), config='digits')
+    val = int(image_to_string(self.imgcrop(img,262,187), config=tsconfig))
     results.append({ 'okres':'Plzeň-sever', 'kraj': self.kraj, 'hodnota':val})
     
-    val = image_to_string(self.imgcrop(img,375,257), config='digits')
+    val = int(image_to_string(self.imgcrop(img,375,257), config=tsconfig))
     results.append({ 'okres':'Rokycany', 'kraj': self.kraj, 'hodnota':val})
     
-    val = image_to_string(self.imgcrop(img,118,370), config='digits')
+    val = int(image_to_string(self.imgcrop(img,118,370), config=tsconfig))
     results.append({ 'okres':'Domažlice', 'kraj': self.kraj, 'hodnota':val})
     
-    val = image_to_string(self.imgcrop(img,322,368), config='digits')
+    val = int(image_to_string(self.imgcrop(img,322,368), config=tsconfig))
     results.append({ 'okres':'Plzeň-jih', 'kraj': self.kraj, 'hodnota':val})
     
-    val = image_to_string(self.imgcrop(img,265,492), config='digits')
+    val = int(image_to_string(self.imgcrop(img,265,492), config=tsconfig))
     results.append({ 'okres':'Klatovy', 'kraj': self.kraj, 'hodnota':val})
 
     return results
