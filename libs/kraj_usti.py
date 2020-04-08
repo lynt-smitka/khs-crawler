5770a2db7088443c6719b876482f0068d8af062a
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-04-08 09:56:29 +0200

    Ústecký kraj: další oprava parsování PDFka…

--- libs/kraj_usti.py
+++ libs/kraj_usti.py
@@ -1,4 +1,5 @@
 from . import utils
+from pdfminer.layout import LAParams
 
 class web:
   kraj= 'Ústecký kraj'
@@ -7,21 +8,18 @@ class web:
     url = 'http://www.khsusti.cz/php/kousky/covid19/pocet_testovanych_osob_na_covid19_ustecky_kraj.pdf'
     pocet_okresu = 7
     results=[]
-    lines = [line for line in utils.get_pdfminer(url) if len(line.replace(' ', '')) > 0]
+    laparams=LAParams()
+    laparams.boxes_flow=None
+    lines = [line for line in utils.get_pdfminer(url, laparams) if len(line.replace(' ', '')) > 0]
     start_index = None
     distance_to_counts = None
     for i, line in enumerate(lines):
-        if line.startswith('okres'):
+        if line.startswith('(kumulativní počet)'):
             start_index = i + 1
-            for next_i in range(start_index, len(lines) - i):
-                if (lines[next_i][0].isdigit()):
-                    distance_to_counts = next_i - i - 1
-                    break
-            break
-    if start_index and distance_to_counts:
-        for i in range(start_index, start_index + pocet_okresu):
-            name = lines[i].strip().replace('D ín', 'Děčín').replace('Litom ice', 'Litoměřice')
-            results.append({'okres': name, 'kraj': self.kraj, 'hodnota': int(lines[i + distance_to_counts].strip())})
+    for i in range(start_index, start_index + pocet_okresu * 3, 3):
+        value = int(lines[i].strip())
+        name = lines[i+2].strip().replace('D ín', 'Děčín').replace('Litom ice', 'Litoměřice')
+        results.append({'okres': name, 'kraj': self.kraj, 'hodnota': value})
 
     return results
 
