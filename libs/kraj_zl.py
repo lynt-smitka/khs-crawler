d81d4adb7288248548d2cf979b08b8a2bb66a3ed
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-04-09 00:14:24 +0200

    Zlínský kraj: parsování PDF s camelot-py

--- libs/kraj_zl.py
+++ libs/kraj_zl.py
@@ -1,30 +1,24 @@
 from . import utils
+import camelot
 from urllib.parse import urljoin
 
-class web:
-  kraj= 'Zlínský kraj'
-  
-  def crawl(self):
-    results = []
-    pocet_okresu = 4
-    url = 'http://www.khszlin.cz/25304-novy-koronavirus-2019-ncov'
-    page = utils.get_url(url)
-    doc = page.select_one('a.pdf[href*=info_cov19]')['href']
-    url = urljoin(url, doc)
-    lines = [line for line in utils.get_pdfminer(url) if len(line.replace(' ', '')) > 0]
-    start_index = None
-    distance_to_counts = None
-    for i, line in enumerate(lines):
-      if line.startswith('Počet osob s onemocněním'):
-        start_index = i + 1
-        for next_i in range(start_index, len(lines) - i):
-          if (lines[next_i][0].isdigit()):
-            distance_to_counts = next_i - i - 1
-            break
-        break
-    if start_index and distance_to_counts:
-      for i in range(start_index, start_index + pocet_okresu):
-        name = lines[i].strip().replace('okres ', '')
-        results.append({'okres': name, 'kraj': self.kraj, 'hodnota': int(lines[i + distance_to_counts].strip())})
 
-    return results
+class web:
+    kraj = "Zlínský kraj"
+
+    def crawl(self):
+        pocet_okresu = 4
+        results = []
+        url = "http://www.khszlin.cz/25304-novy-koronavirus-2019-ncov"
+        page = utils.get_url(url)
+        doc = page.select_one("a.pdf[href*=info_cov19]")["href"]
+        filename = utils.download_file(urljoin(url, doc), ".pdf")
+        tables = camelot.read_pdf(filename)
+        data = tables[0].df.to_dict("records")
+        for row in data[1:1+pocet_okresu]:
+            results.append({
+              "okres": row[0].replace("okres ", ""),
+              "kraj": self.kraj,
+              "hodnota": int(row[1])
+            })
+        return results
