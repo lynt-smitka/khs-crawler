aee53dcce74ce600bb7b857967b34723e878f41a
Jiří Helebrant <jiri.helebrant@nic.cz>
2020-04-09 20:41:45 +0200

    Jihomoravský kraj: další oprava parsování PDF #17

--- libs/kraj_brn.py
+++ libs/kraj_brn.py
@@ -1,39 +1,33 @@
 from . import utils
-# hodně beta
+from urllib.parse import urljoin
+from pdfminer.layout import LAParams
+
 
 class web:
+    kraj = "Jihomoravský kraj"
 
-  kraj= "Jihomoravský kraj"
-  
-  def crawl(self):
-    results=[]
-    page = utils.get_url('http://www.khsbrno.cz/admin/upload/aktuality/?C=M;O=D')
-    links = page.select('td a')
-    doc=''
+    def crawl(self):
+        pocet_okresu = 7
+        results = []
+        url = "http://www.khsbrno.cz/admin/upload/aktuality/?C=M;O=D"
+        page = utils.get_url(url)
+        doc = page.select_one("a[href$=pdf]")["href"]
+        lines = [line for line in utils.get_pdfminer(
+            urljoin(url, doc)) if len(line.replace(" ", "")) > 0]
+        start_index = None
+        distance_to_counts = None
+        for i, line in enumerate(lines):
+            if line.startswith("Počet případů"):
+                start_index = i + 1
+                break
+        for i, line in enumerate(lines):
+            if line.startswith("Okres"):
+                distance_to_counts = i - start_index + 1
+                break
+        for i in range(start_index, start_index + pocet_okresu):
+            value = int(lines[i].strip())
+            name = lines[i+distance_to_counts].strip()
+            results.append(
+                {"okres": name, "kraj": self.kraj, "hodnota": value})
 
-    for link in links:
-        if "14_" in link['href']:
-            doc = link['href']
-            break
-            
-    lines = utils.get_pdfminer('http://www.khsbrno.cz/admin/upload/aktuality/%s'%doc)
-       
-    
-    def calculate_offset(lines):
-      i=0
-      p1=0
-      p2=0
-      for line in lines:
-        if 'Brno-město' in line:
-            p1=i
-        if '(první případy ' in line:
-            p2=i+2
-        i=i+1
-      return (p1,p2)
-
-    offset=calculate_offset(lines)
-
-    for i in range(0,7):
-      results.append({ 'okres':lines[offset[0]+i].strip(), 'kraj': self.kraj, 'hodnota': lines[offset[0]+9+i]})
-
-    return results
+        return results
